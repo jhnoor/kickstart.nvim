1e7279810458668cafb6252472fa456d1c33c49f
jhnoor <jamawadi@gmail.com>
2026-01-15 13:44:12 +0100

    markdown preview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,14 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {'akinsho/git-conflict.nvim', version = "*", config = true}
+  {'akinsho/git-conflict.nvim', version = "*", config = true},
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = 'cd app && npm install',
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreviewToggle<CR>', desc = '[M]arkdown [P]review toggle' },
+    },
+  },
 }
